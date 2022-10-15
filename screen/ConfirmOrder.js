284b75836274d821f3c9a9be469478ff791b16b0
Tanabut <tangg6@outlook.com>
2022-10-16 01:02:22 +0700

    increment & decrement item and cal total

--- screen/ConfirmOrder.js
+++ screen/ConfirmOrder.js
@@ -5,16 +5,17 @@ import {
   SafeAreaView,
   Image,
   ScrollView,
+  TextInput,
 } from 'react-native';
-import React, {useState} from 'react';
-import {Card} from '@rneui/themed';
+import React, {useState,useEffect} from 'react';
+import { Button,Icon,Card } from "@rneui/themed";
 import HeaderTemple from '../components/HeaderTemple';
 import item1 from '../assets/item1.png';
 import item2 from '../assets/item2.png';
 const ConfirmOrder = () => {
   // Mock Data
   const templeName = 'วัดบวรนิเวศราชวรวิหาร';
-  let itemList = [
+  const data = [
     {
       name: 'ชุดตักบาตรใหญ่',
       price: 800,
@@ -28,28 +29,52 @@ const ConfirmOrder = () => {
       img: item2,
     },
     {
-      name: 'ชุดสังฆทาน',
-      price: 150,
+      name: 'ชุดอาหารปลา',
+      price: 40,
       amount: 1,
       img: item2,
     },
     
+    
   ];
 
+  
+  const [cartItem, setcartItem] = useState(data)
+
   const [total, settotal] = useState(
-    itemList.reduce((pre, cur) => {
-      console.log(pre);
+    cartItem.reduce((pre, cur) => {
       return pre + cur.price * cur.amount;
     }, 0),
-  );
+    );
+  
+  const increment = (name) => {  
+    const itemAmount = cartItem.find(item => item.name === name)
+    setcartItem(cartItem.map((i) => i.name === name ? {...itemAmount, amount: i.amount+1}: i))
+    calTotal()
+  }
 
+  const decrement = (name) => {  
+    const itemAmount = cartItem.find(item => item.name === name)
+    setcartItem(cartItem.map((i) => i.name === name ? {...itemAmount, amount: i.amount-1}: i))
+    calTotal()
+
+  }
+  
+  const calTotal = () => {
+    settotal(cartItem.reduce((pre, cur) => {
+      return pre + cur.price * cur.amount;
+    }, 0),)
+  }
+  console.log(cartItem)
+  console.log(total);
+  
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView
         style={styles.scrollCont}
         showsVerticalScrollIndicator={false}>
         <HeaderTemple temple={templeName} />
-        {itemList.map((items, i) => {
+        {cartItem.map((items, i) => {
           () => {
             settotal(total + items.price * items.amount);
           };
@@ -63,7 +88,9 @@ const ConfirmOrder = () => {
                   <View style={styles.textCont}>
                     <Text style={styles.text}>{items.name}</Text>
                     <Text style={styles.text}>{items.price} ฿</Text>
-                    <Text style={styles.text}>{items.amount}</Text>
+                    <Button onPress={()=> decrement(items.name)}> - </Button>
+                    <TextInput style={styles.textInput} onChange>{items.amount}</TextInput>
+                    <Button onPress={()=> increment(items.name)}></Button>
                   </View>
                 </View>
               </Card>
@@ -92,6 +119,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff',
+    width:'100%',
+    height:'100%',
   },
 
   scrollCont: {
@@ -137,4 +166,14 @@ const styles = StyleSheet.create({
     fontFamily: 'Kanit',
     color: '#000',
   },
+
+  textInput:{
+    backgroundColor:'#fff',
+    fontSize: 16,
+    fontFamily: 'Kanit',
+    color: '#000',
+    width:30,
+    padding:2,
+    borderRadius:10,
+  }
 });
