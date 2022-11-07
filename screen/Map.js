b4267b1bf889de9d90cce4dd1d5fb0be3ef69bae
suttipong <por.papika@gmail.com>
2022-11-07 17:37:11 +0700

    new api

--- screen/Map.js
+++ screen/Map.js
@@ -1,5 +1,5 @@
-import React,{ useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useRef } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 import MapView from "react-native-maps";
 import Geolocation from "@react-native-community/geolocation";
 
@@ -11,11 +11,54 @@ function getMyLocation(){
   if (myPos == data){
     Geolocation.getCurrentPosition(info => setMypos([info.coords.latitude,info.coords.longitude]));
   }
-    console.log('My Location')
-    console.log(myPos)
+    
   return myPos
 }
 export default function Map() {
+  const mapRef = useRef(null);
+  const [latitude, setMylatitude] = useState(null);
+  const [longitude, setMylongitude] = useState(null);
+
+  const [region, setRegion] = useState({
+    latitude: latitude,
+    longitude: longitude,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+
+  const tokyoRegion = {
+    latitude: latitude,
+    longitude: longitude,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+};
+  const goTolocation = () => {
+    //Animate the user to new region. Complete this animation in 3 seconds
+    mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
+  };
+
+  const goToTokyo = () => {
+    //Animate the user to new region. Complete this animation in 3 seconds
+    mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
+  };
+
+  const [position, setPosition] = useState(null);
+
+  const getCurrentPosition = () => {
+    Geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition(JSON.stringify(pos));
+        console.log(position);
+        setMylatitude(pos.coords.latitude);
+        setMylongitude(pos.coords.longitude);
+        console.log(latitude);
+        console.log(longitude);
+      },
+      (error) => Alert.alert('GetCurrentPosition Error', JSON.stringify(error)),
+      { enableHighAccuracy: true }
+    );
+  };
+
   const myloc = getMyLocation();
 
   console.log(myloc)
@@ -23,15 +66,19 @@ export default function Map() {
     <View style={styles.container}>
       {/*Render our MapView*/}
       <MapView
+        ref={mapRef}
         style={styles.map}
         //specify our coordinates.
         initialRegion={{
-          latitude: myloc[0],
-          longitude: myloc[1],
+          latitude: 14.060567,
+          longitude: 100.596508,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        onRegionChangeComplete={(region) => setRegion(region)}
       />
+      <Button onPress={() => goTolocation()} title="Go to Tokyo" />
+      <Button title="Get Current Position" onPress={getCurrentPosition} />
     </View>
   );
 }
