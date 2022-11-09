d766e6e3b3ebd8e0beab697952f7c3f86e456656
suttipong <por.papika@gmail.com>
2022-11-10 05:30:01 +0700

    Add image marker on my location

--- screen/Map.js
+++ screen/Map.js
@@ -1,87 +1,40 @@
 import React, { useState, useRef } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import MapView from "react-native-maps";
-import Geolocation from "@react-native-community/geolocation";
+import MapView,{ Marker } from "react-native-maps";
+import { SafeAreaView , Image} from "react-native-safe-area-context";
 
 
-
-function getMyLocation(){
-  const data = [0,0];
-  const [myPos, setMypos] = useState(data);
-  if (myPos == data){
-    Geolocation.getCurrentPosition(info => setMypos([info.coords.latitude,info.coords.longitude]));
-  }
-    
-  return myPos
-}
-export default function Map() {
-  const mapRef = useRef(null);
-  const [latitude, setMylatitude] = useState(null);
-  const [longitude, setMylongitude] = useState(null);
-
-  const [region, setRegion] = useState({
-    latitude: latitude,
-    longitude: longitude,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
-  });
-
-  const tokyoRegion = {
-    latitude: latitude,
-    longitude: longitude,
-  latitudeDelta: 0.01,
-  longitudeDelta: 0.01,
-};
-  const goTolocation = () => {
-    //Animate the user to new region. Complete this animation in 3 seconds
-    mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
-  };
-
-  const goToTokyo = () => {
-    //Animate the user to new region. Complete this animation in 3 seconds
-    mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
-  };
-
-  const [position, setPosition] = useState(null);
-
-  const getCurrentPosition = () => {
-    Geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition(JSON.stringify(pos));
-        console.log(position);
-        setMylatitude(pos.coords.latitude);
-        setMylongitude(pos.coords.longitude);
-        console.log(latitude);
-        console.log(longitude);
-      },
-      (error) => Alert.alert('GetCurrentPosition Error', JSON.stringify(error)),
-      { enableHighAccuracy: true }
-    );
-  };
-
-  const myloc = getMyLocation();
-
-  console.log(myloc)
-  return (
-    <View style={styles.container}>
-      {/*Render our MapView*/}
-      <MapView
-        ref={mapRef}
+const Map = ()=>{
+  return(
+  <SafeAreaView style={styles.container}>
+      <MapView 
         style={styles.map}
-        //specify our coordinates.
         initialRegion={{
           latitude: 14.060567,
           longitude: 100.596508,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        
         onRegionChangeComplete={(region) => setRegion(region)}
-      />
-      <Button onPress={() => goTolocation()} title="Go to Tokyo" />
-      <Button title="Get Current Position" onPress={getCurrentPosition} />
-    </View>
-  );
+      >
+      <Marker
+        coordinate={{ latitude : 14.060567 , longitude : 100.596508 }}
+        image = {require("../assets/marker01.png")}
+        />
+          {/* <Image 
+            source ={require("../assets/marker01.png")} 
+          />
+
+      </Marker> */}
+        
+      </MapView>
+    </SafeAreaView>
+    );
 }
+
+export default Map;
+
 //create our styling code:
 const styles = StyleSheet.create({
   container: {
