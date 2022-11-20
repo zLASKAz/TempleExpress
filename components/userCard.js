032583474f5d2f2695786ddf606855388258d7a5
KNZ-Yuuto <99586802+KNZ-Yuuto@users.noreply.github.com>
2022-11-20 21:47:27 +0700

    userCard Update

--- components/userCard.js
+++ components/userCard.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { Touchable } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import {Title, View, Text, StyleSheet, Image} from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 const userCard = () => {
     return (
-        <View style={styles.cardContainer}>
-            <Text style={styles.userStyle}>Username</Text>
-            <Text style={styles.numberStyle}>090-XXX-XXXX</Text>
+        <View style={{width:'100%', alignContent:'center'}}>
+            <View style={{width:'100%', flexDirection:'row',paddingTop:30,paddingHorizontal:20}}>
+                    <Image
+                        style = {{width:100,height:100,borderRadius:100/2,backgroundColor:'#ffa646', resizeMode:'contain'}}
+                        source={require('../assets/Profile_Pic.png')}
+                        
+                    />
+                <View style={{width:'100%', flexDirection:'column', alignSelf:'center', padding:10}}>
+                    <Text style={{fontFamily:'Kanit',fontSize:20, color:'#ff780c'}}>username</Text>
+                    <TouchableOpacity>
+                        <Text style={{fontFamily:'Kanit', fontSize:16}}>Edit Profile {'>'} </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
